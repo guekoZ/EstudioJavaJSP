55eee211e04b9bfa70511769146f280e6e85c174
gueko1 <gueko1@gmail.com>
2025-08-15 19:37:04 -0600

    2do cambio de laptop 15/07/25

---
+++ src/java/metodos/java/calcula.java
@@ -0,0 +1,28 @@
+/*
+ 
+ */
+package metodos.java;
+
+public class calcula {
+    
+     private static int resultado;
+
+            public static int suma(int a, int b) {
+
+                resultado = a + b;
+                return resultado;
+            }
+
+            public static int resta(int a, int b) {
+
+                resultado = a - b;
+                return resultado;
+            }
+
+             public static int multiplicacion(int a, int b) {
+
+                resultado = a * b;
+                return resultado;
+            }
+    
+}
